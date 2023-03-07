bf091b07556331b0c01e044b2c2cca0061eb1372
Manuel <manuelmat41@gmail.com>
2023-03-07 17:20:56 +0100

    fix: refactor

--- addons/StateMachine/DefaultStateMachineNode.cs
+++ addons/StateMachine/DefaultStateMachineNode.cs
@@ -3,14 +3,15 @@ using System;
 
 public partial class DefaultStateMachineNode : Node
 {
-    public DefaultStateMachine<Node, State<Node>> DefaultStateMachine { get; set; }
+    private DefaultStateMachine<Node, State<Node>> defaultStateMachine;
+    public DefaultStateMachine<Node, State<Node>> DefaultStateMachine { get => defaultStateMachine; }
     public override void _Ready()
     {
-        DefaultStateMachine = new DefaultStateMachine<Node, State<Node>>(GetParent());
+        defaultStateMachine = new DefaultStateMachine<Node, State<Node>>(GetParent());
     }
 
     public override void _Process(double delta)
     {
-        DefaultStateMachine.Update();
+        defaultStateMachine.Update();
     }
 }
