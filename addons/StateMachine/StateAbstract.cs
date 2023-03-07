3cac82ab9deb5bd7f3fce29fd1cf09f8d752582c
manuel <manuelmat41@gmail.com>
2023-03-07 12:26:01 +0100

    feat: added StateMachine, StateAbstract and GlobalState classes

--- addons/StateMachine/StateAbstract.cs
+++ addons/StateMachine/StateAbstract.cs
@@ -1,15 +1,43 @@
-using Godot;
 using System;
 
-public partial class StateAbstract : Node
+/// <summary>
+/// Estado abstracto que ejecuta difentes funciones en una State Machine al entrar, al salir o ir actualizandose para procesar diferentes parametros de la entidad T
+/// </summary>
+/// <typeparam name="T">Parametro generico que representa una entidad</typeparam>
+public abstract class StateAbstract<T> where T : class
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private string machineOwnerName;
+    /// <summary>
+    /// Nombre de la State Machine a la que pertenece el estado 
+    /// </summary>
+    public string MachineOwnerName
+    {
+        get => machineOwnerName;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("The name of the state machine doesnt exist");
+            }
+
+            machineOwnerName = value;
+        }
+    }
+    /// <summary>
+    /// Funcion que se ejecuta al establecer el estado en la State Machine
+    /// </summary>
+    public abstract void Enter(T entity) where T : class;
+	/// <summary>
+	/// Funcion que se ejecuta al actualizar el estado en la State Machine para observar si se quiere transicionar a otro estado
+	/// </summary>
+    public abstract void UpdateTransition(T entity) where T : class;
+	/// <summary>
+	/// Funcion que se ejecuta al actualizar el estado en la State Machine para observar si se quiere actualizar algunas fisicas de la entidad
+	/// </summary>
+    public abstract void UpdatePhysics(T entity, float delta) where T : class;
+	// <summary>
+    /// Funcion que se ejecuta al quitar el estado en la State Machine
+    /// </summary>
+    public abstract void Exit(T entity) where T : class;
 }
--- addons/StateMachine/StateMachine.cs
+++ addons/StateMachine/StateMachine.cs
@@ -1,20 +1,87 @@
-using Godot;
 using System;
-
-public partial class StateMachine : Node
+/// <summary>
+/// Maquina que cambia entre diferentes la entidad T
+/// </summary>
+/// <typeparam name="T">Representa una entidad genrica</typeparam>
+public class StateMachine<T> where T : class
 {
-	private StateAbstract CurrentState;
-	private StateAbstract PreviousState;
-	private StateAbstract NextState;
-	private StateAbstract InitState;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    /// <summary>
+    /// Estado actual que se esta actualizando
+    /// </summary>
+    private StateAbstract currentState;
+    /// <summary>
+    /// Estado previo al actual
+    /// </summary>
+    private StateAbstract previousState;
+    /// <summary>
+    /// Estado que se va a cambiar por el actual
+    /// </summary>
+    private StateAbstract nextState;
+    /// <summary>
+    /// Estado global que se actualiza constantemente solo para observar parametros
+    /// </summary>
+    private GlobalState? globalState;
+    /// <summary>
+    /// Paremtro que representa una clase de entidad generica
+    /// </summary>
+    private T entity;
+    private string name;
+    /// <summary>
+    /// Nombre de la State Machine
+    /// </summary>
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("The name is empty or null");
+            }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+            name = value;
+        }
+    }
+
+    public StateMachine(string name, StateAbstract initState, T entity, StateAbstract globalState)
+    {
+        Name = name;
+        currentState = initState;
+        ChangeState(initState);
+        this.entity = entity;
+        this.globalState = globalState;
+    }
+
+    /// <summary>
+    /// Cambia el estado actual por el pasado por parametro ejecutando las funciones correspondientes de los estados
+    /// </summary>
+    /// <param name="state">Esatdo que va a remplaazar al actual</param>
+    public void ChangeState(StateAbstract state)
+    {
+        // Set the next state and execute Enter function
+        nextState = state;
+        nextState.Enter(entity);
+
+        // Set the previous state and exeute Exit function
+        previousState = currentState;
+        currentState.Exit(entity);
+
+        // Set the current state and remove next state
+        currentState = nextState;
+        nextState = null;
+    }
+
+    /// <summary>
+    /// Actualiza el estado actual como el global
+    /// </summary>
+    /// <param name="delta">Unidad de tiempo de ejecucion del programa</param>
+    public void UpdateState(float delta)
+    {
+        globalState?.Update();
+        // See if the state want to change
+        currentState.UpdateTransition(entity);
+
+        // Update the physic linked to the state
+        currentState.UpdatePhysics(entity, delta);
+    }
 }
